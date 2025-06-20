fab82bc82b55681d14d144e55f0dc6d2d56e3e54
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2025-06-20 13:35:28 +0530

    Initial Commit for Payment and Delivery service.

---
+++ paymentDeliveryService/src/test/java/com/example/paymentDeliveryService_microservice/paymentDeliveryService/PaymentDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentDeliveryService_microservice.paymentDeliveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
